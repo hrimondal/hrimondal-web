bb1f625bc93d50b3de9581ae741f0637b702e5a3
H. Mondal <hmondal.bd@gmail.com>
2024-12-12 17:30:26 +0600

    Update navigation.ts

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,34 +1,35 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
-  { name: "Services", url: "/services" },
+//  { name: "Projects", url: "/products" },
+//  { name: "Services", url: "/services" }, 
   { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
+//  {
+//    section: "Ecosystem",
+//    links: [
+//      { name: "Documentation", url: "/welcome-to-docs/" },
+//      { name: "Projects", url: "/products" },
+//      { name: "Services", url: "/services" },
+//    ],
+//  },
   {
-    section: "Ecosystem",
+    section: "STEAMer Academy",
     links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
-    ],
-  },
-  {
-    section: "Company",
-    links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
+      { name: "About Our Team", url: "https://steameracademy.me/about/" },
+      { name: "Blogs", url: "https://steameracademy.me/blogs/" },
+//      { name: "Careers", url: "#" },
+      { name: "Contact", url: "https://steameracademy.me/contact/" },
     ],
   },
 ];
 // An object of links for social icons
 const socialLinks = {
   facebook: "https://www.facebook.com/thathrimondal",
+  instagram: "https://www.instagram.com/hrimondal/",
   x: "https://x.com/thehmondal",
   github: "https://github.com/hrimondal",
   google: "https://www.google.com/",
